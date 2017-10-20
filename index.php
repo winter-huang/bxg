384f924d36124da0c7dd099b97e3cf1c7ace426f
winter-huang <894149653@qq.com>
2017-10-20 20:54:20 +0800

    路由导航文件设置完成

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
   header("Content-type:text/html;charset=utf-8");
-  echo include '/src/dashboard/index.html';
+  //设置路由导航
+  //include '/src/dashboard/index.html';
+  //echo $_SERVER['PATH_INFO'];
+  //var_dump($_SERVER);
+  $path = '';
+  if(array_key_exists('PATH_INFO',$_SERVER)){
+    $path = $_SERVER['PATH_INFO'];
+    $path = substr($path,1);
+    $pathArr = explode('/',$path);
+    if(count($pathArr) == 2){
+        $path = '/src/' . $path;
+    }else if(count($pathArr) == 1){
+        $path = '/src/dashboard/' . $path;
+    }else{
+        $path = '/src/dashboard/index';
+    }
+    include $path . '.html';
+
+  }
 ?>
